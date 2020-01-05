0981769b62b953132bf1fcaa776812600e46545b
jordancarson <jordanlouiscarson@gmail.com>
2020-01-05 14:49:54 -0500

    uploading LinkedList, double-linked list append solution

--- Udacity/practice_problems/linked_list/data_structure.py
+++ Udacity/practice_problems/linked_list/data_structure.py
@@ -39,6 +39,124 @@ class LinkedList:
         return out_list
 
 
+class LinkedListPractice:
+    def __init__(self):
+        self.head = None
+
+    def prepend(self, value):
+        """ Prepend a value to the beginning of the list. """
+
+        # TODO: Write function to prepend here
+
+        if self.head is None:
+            self.head = Node(value)
+            return
+
+        new_head = Node(value)
+        new_head.next = self.head
+        self.head = new_head
+
+    def append(self, value):
+        """ Append a value to the end of the list. """
+        if self.head is None:
+            self.head = Node(value)
+            return
+
+        # move to the tail
+        node = self.head
+        while node.next:
+            node = node.next
+
+        node.next = Node(value)
+
+    def search(self, value):
+        """ Search the linked list for a node with the requested value and return the node. """
+
+        # TODO: Write function to search here
+        if self.head is None:
+            return None
+
+        node = self.head
+        while node:
+            if node.value == value:
+                return node
+            node = node.next
+        # if we get here the value does not exist
+        raise ValueError("Value does not exist in the LinkedList")
+
+    def remove(self, value):
+        """ Remove first occurrence of value. """
+
+        # TODO: Write function to remove here
+        if self.head is None:
+            return
+
+            # head node is not None, test to see if the head value equals the value to remove
+        if self.head.value == value:
+            self.head = self.head.next
+            return
+        node = self.head
+
+        while node.next:
+            if node.next.value == value:
+                node.next = node.next.next
+                return
+            node = node.next
+
+        raise ValueError("Value does not exist in the list.")
+
+    def pop(self):
+        """ Return the first node's value and remove it from the list. """
+        if self.head is None:
+            return None
+        node = self.head
+        self.head = self.head.next
+        return node.value
+
+    def insert(self, value, pos):
+        """ Insert value at pos position in the list. If pos is larger than the
+            length of the list, append to the end of the list. """
+
+        # TODO: Write function to insert here
+        if pos == 0:
+            self.prepend(value)
+            return
+
+        index = 0
+        node = self.head
+        while node.next and index <= pos:
+            if (pos - 1) == index:
+                new_node = Node(value)
+                new_node.next = node.next
+                node.next = new_node
+                return
+
+            index += 1
+            node = node.next
+        else:
+            self.append(value)
+        # pass
+
+    def size(self):
+        """ Return the size or length of the linked list. """
+
+        # TODO: Write function to get size here
+        size = 0
+        node = self.head
+        while node:
+            size += 1
+            node = node.next
+        return size
+
+    def to_list(self):
+        out = []
+        node = self.head
+        while node:
+            out.append(node.value)
+            node = node.next
+        return out
+
+
 class DoublyLinkedList:
     def __init__(self):
         self.head = None
@@ -85,4 +203,51 @@ if __name__ == '__main__':
         print(node.value)
         node = node.previous
 
+    # Test prepend
+    linked_list = LinkedListPractice()
+    linked_list.prepend(1)
+    assert linked_list.to_list() == [1], f"list contents: {linked_list.to_list()}"
+    linked_list.append(3)
+    linked_list.prepend(2)
+    assert linked_list.to_list() == [2, 1, 3], f"list contents: {linked_list.to_list()}"
+    print('Pass: LinkedList Prepend.\n' if (linked_list.to_list() == [2, 1, 3]) else 'False')
 
+    # Test append
+    linked_list = LinkedListPractice()
+    linked_list.append(1)
+    assert linked_list.to_list() == [1], f"list contents: {linked_list.to_list()}"
+    linked_list.append(3)
+    assert linked_list.to_list() == [1, 3], f"list contents: {linked_list.to_list()}"
+    print('Pass: LinkedList Append.\n' if (linked_list.to_list() == [1, 3]) else 'False')
+    # Test search
+    linked_list.prepend(2)
+    linked_list.prepend(1)
+    linked_list.append(4)
+    linked_list.append(3)
+    assert linked_list.search(1).value == 1, f"list contents: {linked_list.to_list()}"
+    assert linked_list.search(4).value == 4, f"list contents: {linked_list.to_list()}"
+
+
+    # Test remove
+    linked_list.remove(1)
+    assert linked_list.to_list() == [2, 1, 3, 4, 3], f"list contents: {linked_list.to_list()}"
+    linked_list.remove(3)
+    assert linked_list.to_list() == [2, 1, 4, 3], f"list contents: {linked_list.to_list()}"
+    linked_list.remove(3)
+    assert linked_list.to_list() == [2, 1, 4], f"list contents: {linked_list.to_list()}"
+
+    # Test pop
+    value = linked_list.pop()
+    assert value == 2, f"list contents: {linked_list.to_list()}"
+    assert linked_list.head.value == 1, f"list contents: {linked_list.to_list()}"
+
+    # Test insert
+    linked_list.insert(5, 0)
+    assert linked_list.to_list() == [5, 1, 4], f"list contents: {linked_list.to_list()}"
+    linked_list.insert(2, 1)
+    assert linked_list.to_list() == [5, 2, 1, 4], f"list contents: {linked_list.to_list()}"
+    linked_list.insert(3, 6)
+    assert linked_list.to_list() == [5, 2, 1, 4, 3], f"list contents: {linked_list.to_list()}"
+
+    # Test size
+    assert linked_list.size() == 5, f"list contents: {linked_list.to_list()}"
