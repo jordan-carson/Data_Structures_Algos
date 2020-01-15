d85a942e0765dab3edfe0c7ff601fee6b3100d3e
jordancarson <jordanlouiscarson@gmail.com>
2020-01-15 00:42:42 -0500

    adding solution

--- Udacity/Project 2/Problem 3.py
+++ Udacity/Project 2/Problem 3.py
@@ -1,3 +1,6 @@
+import heapq
+
+
 class HuffmanNode:
     def __init__(self, char, frequency):
         self.char = char
@@ -38,23 +41,42 @@ class HuffmanCoding:
             Decode the text from its compressed form.
             You then will need to create encoding, decoding, and sizing schemas.
     """
+    def __init__(self):
+        self.data = dict()
+
     def encode(self):
         pass
 
     def decode(self):
         pass
 
+    # def search(self):
+
     def _create_frequencies(self, text, as_dict=False):
+        """Take a string and determine the relevant frequencies of the characters."""
         freq = dict()
         for ch in text:
             if ch not in freq:
                 freq[ch] = 1
             else:
                 freq[ch] += 1
-        return [HuffmanNode(temp_ch, temp_freq) for temp_ch, temp_freq in freq.items()] if not as_dict else freq
+        # return [HuffmanNode(temp_ch, temp_freq) for temp_ch, temp_freq in freq.items()] if not as_dict else freq
+        return freq
 
     def _sort_frequencies(self, text):
-        return sorted(self._create_frequencies(text), key=lambda x: x.freq, reverse=True)
+        import operator
+
+        dic = self._create_frequencies(text)
+        sorted_d = sorted(dic, key=dic.get, reverse=True)
+        new_dict = dict()
+        for val in sorted_d:
+            new_dict[val] = dic[val]
+        return new_dict
+        # tup = ()
+        # sorted_list = sorted(self._create_frequencies(text), key=lambda x: x.freq, reverse=True)
+        # for ch, freq in self._create_frequencies(text).items():
+        # return sorted_d
+        # return sorted(self._create_frequencies(text), key=lambda x: x.freq, reverse=True)
 
 
 def text_to_frequency_dict(text):
@@ -65,9 +87,11 @@ def text_to_frequency_dict(text):
         else:
             freq_dict[char] += 1
     for char, freq in freq_dict.items():
-        final.append(HuffmanCodingNode(char, freq))
+        final.append(HuffmanNode(char, freq))
     return final
 
 
 if __name__ == '__main__':
     text_to_frequency_dict('jordan')
+
+    print(HuffmanCoding()._sort_frequencies('jordan carson'))
