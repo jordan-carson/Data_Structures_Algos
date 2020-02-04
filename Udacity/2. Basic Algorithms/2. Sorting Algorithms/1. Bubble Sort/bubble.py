48ddca080554eef50c7de874ef8821373fef9988
jordancarson <jordanlouiscarson@gmail.com>
2020-02-03 21:48:13 -0500

    adding solution

--- Udacity/2. Basic Algorithms/2. Sorting Algorithms/1. Bubble Sort/bubble.py
+++ Udacity/2. Basic Algorithms/2. Sorting Algorithms/1. Bubble Sort/bubble.py
@@ -22,7 +22,6 @@ def bubble_sort(arr):
         for j in range(0, len(arr)-1):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
-                # swap(arr[j], arr[j+1])
     return arr
 
 
@@ -44,3 +43,21 @@ def bubbleSort(arr):
 
     return arr
 print(bubble_sort([2,3,4,3, 1, 0]))
+
+# Entries are (h, m) where h is the hour and m is the minute
+sleep_times = [(24, 13), (21, 55), (23, 20), (22, 5), (24, 23), (21, 58), (24, 3)]
+
+
+def bubble_sort_2(l):
+    for i in range(len(l)):
+        for j in range(1, len(l)):
+            this_hour, this_min = l[j]
+            prev_hour, prev_min = l[j - 1]
+            if this_hour < prev_hour or (prev_hour == this_hour and prev_min > this_min):
+                continue
+            l[j], l[j - 1] = (prev_hour, prev_min), (this_hour, this_min)
+    return l
+
+
+bubble_sort_2(sleep_times)
+print("Pass" if (sleep_times == [(24, 23), (24, 13), (24, 3), (23, 20), (22, 5), (21, 58), (21, 55)]) else "Fail")
