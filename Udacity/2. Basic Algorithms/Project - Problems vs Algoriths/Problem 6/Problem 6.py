9bc072f52ffe6a6e2b3f913403dfe7b0f38cb3a6
jordancarson <jordanlouiscarson@gmail.com>
2020-02-12 19:51:34 -0500

    adding solution - Project 2 DS&Algo

---
+++ Udacity/2. Basic Algorithms/Project - Problems vs Algoriths/Problem 6/Problem 6.py
@@ -0,0 +1,28 @@
+def get_min_max(ints):
+    """
+    Return a tuple(min, max) out of list of unsorted integers.
+
+    Args:
+       ints(list): list of integers containing one or more integers
+    """
+    current_max = ints[0]
+    current_min = ints[0]
+
+    for i in range(len(ints)):
+        if current_max > ints[i]:
+            current_max = ints[i]
+
+        if current_min < ints[i]:
+            current_min = ints[i]
+
+    return current_max, current_min
+
+
+## Example Test Case of Ten Integers
+import random
+
+l = [i for i in range(0, 10)]  # a list containing 0 - 9
+random.shuffle(l)
+
+print ("Pass" if ((0, 9) == get_min_max(l)) else "Fail")
+
