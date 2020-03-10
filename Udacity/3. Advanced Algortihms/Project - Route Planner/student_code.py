b2a5bd3237946c377b5fb196135e4921f4e7e3cb
jordancarson <jordanlouiscarson@gmail.com>
2020-03-10 00:45:31 -0400

    Pre-final solution

--- Udacity/3. Advanced Algortihms/Project - Route Planner/student_code.py
+++ Udacity/3. Advanced Algortihms/Project - Route Planner/student_code.py
@@ -1,37 +1,84 @@
 # https://www.redblobgames.com/pathfinding/a-star/introduction.html
 # https://github.com/fafl/priority-queue/blob/master/priority_queue.py
 
-from .priority_queue import PriorityQueue
+# from .priority_queue import PriorityQueue
+import heapq
+
+"""
+
+For every path finding operation we have a GRAPH (M), a starting node (start), and an end node (goal). 
+We need to find the most optimal / shortest path from the starting node to the end node within the given GRAPH.
+
+Pseudocode for A* a.k.a A_Star
+
+OPEN // set of node to be evaluated
+CLOSED // set of nodes already evaluated
+
+loop
+    current = node in OPEN with the lowest f_cost (videos say the cost starts at 0).
+    remove current from OPEN
+    add current to CLOSED
+    
+    if current is equal to the target
+        return 
+    
+    foreach neighbor of the current node
+        if neighbor is not traversable or neighbor is in CLOSED
+            skip to the next neighbor
+        
+        if new path to neighbor is shorter OR neighbor is not in OPEN
+            set the f_cost of the neighbor
+            set the parent of neighbor to current
+            if neighbor is not in OPEN
+                add neighbor to open 
+"""
 
 
-def manhattan_distance(b, a):
+def heuristic_sqrt(origin, destination):
+    return ((origin[0] - destination[0]) ** 2 + (origin[1] - destination[1]) ** 2) ** (1 / 2)
+
+
+def heuristic(origin, destination):
+    return ((origin[0] - destination[0]) ** 2 + (origin[1] - destination[1]) ** 2)
+
+
+def manhattan_distance(a, b):
     return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
 
 def shortest_path(M, start, goal):
-    return a_star_search(M, start, goal)
+    return a_star(M, start, goal)
 
 
 def dikstras(graph, start, end):
     raise NotImplementedError
 
 
-def a_star_search(graph, start, goal):
+def a_star(graph, start, goal):
     came_from = dict()
     came_from[start] = None
 
-    frontier = PriorityQueue()
-    frontier.insert((start, 0))
+    cost_dict = dict()
+    cost_dict[start] = 0
+    # frontier = PriorityQueue()
+    # frontier.insert((start, 0))
+    frontier = [(0, start)]
 
-    while not frontier.empty():
-        # current = frontier.()
-        # current =
-
+    while len(frontier) > 0:
+        current = heapq.heappop(frontier)[1]
         if current == goal:
             break
 
-        for next in graph.neighbors(current):
-            if next not in came_from:
-                priority = heuristic(goal, next)
-                frontier.insert(next, priority)
+        for next in graph.roads(current):
+
+            # we need to calculate the path cost now
+            cpath_cost = heuristic(graph.intersections[current], graph.intersections[next])
+            new_cost = cost_dict[current] + cpath_cost
+
+            if next not in cost_dict or new_cost < cost_dict[next]:
                 came_from[next] = current
+                cost_dict[next] = new_cost
+                heapq.heappush(frontier, ())
+                # priority = heuristic(goal, next)
+                # frontier.insert(next, priority)
+                # came_from[next] = current
